6802cd32e019c07ab2b4ff1257c582a8beca5fc0
David Gregorio Tam Fung <David@MacBook-Pro-de-David.local>
2018-12-23 21:40:34 -0400

    prueba

--- SD-Proyecto-Servidor/src/sd/proyecto/servidor/SDProyectoServidor.java
+++ SD-Proyecto-Servidor/src/sd/proyecto/servidor/SDProyectoServidor.java
@@ -4,18 +4,42 @@
  * and open the template in the editor.
  */
 package sd.proyecto.servidor;
-
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 /**
  *
  * @author David
  */
 public class SDProyectoServidor {
-
+    
+    final static String INET_ADDR = "230.0.0.0";
+    final static int PORT = 8888;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
+        // Get the address that we are going to connect to.
+        InetAddress addr = InetAddress.getByName(INET_ADDR);
+     
+        // Open a new DatagramSocket, which will be used to send the data.
+        try (DatagramSocket serverSocket = new DatagramSocket()) {
+            for (int i = 0; i < 10; i++) {
+                String msg = "Sent message no " + i;
+
+                // Create a packet that will contain the data
+                // (in the form of bytes) and send it.
+                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
+                        msg.getBytes().length, addr, PORT);
+                serverSocket.send(msgPacket);
+     
+                System.out.println("Server sent packet with msg: " + msg);
+                Thread.sleep(500);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-    
 }
